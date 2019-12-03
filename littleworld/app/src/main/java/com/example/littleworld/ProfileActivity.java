ba6d53a013e705a1fa97068864b24dbf649aab5b
Messier42 <yuezhang1999@hotmail.com>
2019-12-03 16:08:29 +0800

    Add a new activity

---
+++ littleworld/app/src/main/java/com/example/littleworld/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.littleworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_main);
+    }
+}
