a11eb0b20dfeac6729968af390064e5f27cfb3c7
HELLO <870029686@qq.com>
2019-12-07 17:21:54 +0800

    定位“

--- littleworld/app/src/main/java/com/example/littleworld/ReleaseDynamics.java
+++ littleworld/app/src/main/java/com/example/littleworld/ReleaseDynamics.java
@@ -29,6 +29,14 @@ import androidx.core.content.FileProvider;
 import java.io.File;
 import java.io.IOException;
 
+import android.widget.TextView;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationClientOption.AMapLocationMode;
+import com.amap.api.location.AMapLocationListener;
+
 public class ReleaseDynamics extends AppCompatActivity {
     public static final int TAKE_PHOTO = 1;
 
@@ -40,10 +48,57 @@ public class ReleaseDynamics extends AppCompatActivity {
 
     Bitmap bitmap;
 
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption = null;
+    //声明定位回调监听器
+    public AMapLocationListener mLocationListener = new AMapLocationListener(){
+        @Override
+        public void onLocationChanged(AMapLocation amapLocation) {
+            if (amapLocation != null) {
+                if (amapLocation.getErrorCode() == 0) {
+                    //可在其中解析amapLocation获取相应内容。
+                    String address = new String(amapLocation.getAddress());//解析地址获取详细地址
+                    String province = new String(amapLocation.getProvince());//省份信息
+                    String city = new String(amapLocation.getCity());//城市信息
+                    String block = new String(amapLocation.getDistrict());//城区信息
+                    String street = new String(amapLocation.getStreet());//街道信息
+                    String number = new String(amapLocation.getStreetNum());//街道门牌号信息
+
+                    TextView text1=(TextView)findViewById(R.id.add_place);
+                    text1.append(address);
+
+                    deactivate();//定位成功就停止定位
+                }else {
+                    //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                    String errText = "定位失败," + amapLocation.getErrorCode()+ ": " + amapLocation.getErrorInfo();
+                    TextView text=(TextView)findViewById(R.id.add_place);
+                    text.append("定位错误");
+                    Log.e("AmapError","location Error, ErrCode:"
+                            + amapLocation.getErrorCode() + ", errInfo:"
+                            + amapLocation.getErrorInfo());
+
+                    deactivate();//停止定位
+                }
+            }
+        }
+    };
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.release_dynamics);
+
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mLocationListener);
+        //初始化定位
+        initLocation();
+
         //复选+按钮
         final ImageButton add_pictures=findViewById(R.id.add_pictures);
         //显示图片
@@ -65,6 +120,31 @@ public class ReleaseDynamics extends AppCompatActivity {
                 ReleaseDynamics.this.finish();
             }
         });
+    }
+
+    //初始化定位
+    public void initLocation(){
+
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+
+        //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+        mLocationOption.setLocationMode(AMapLocationMode.Hight_Accuracy);
+
+        //设置定位模式为AMapLocationMode.Battery_Saving，低功耗模式。
+        mLocationOption.setLocationMode(AMapLocationMode.Battery_Saving);
+
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.setNeedAddress(true);
+
+        //设置请求超时时间，单位是毫秒，默认30000毫秒，建议超时时间不要低于8000毫秒。
+        mLocationOption.setHttpTimeOut(20000);
+
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mLocationClient.startLocation();
+
     }
 
     private void showPopupMenu(View view) {
@@ -232,5 +312,51 @@ public class ReleaseDynamics extends AppCompatActivity {
             Toast.makeText(this, "failed to get image", Toast.LENGTH_SHORT).show();
         }
     }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        deactivate();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    /**
+     * 停止定位
+     */
+    public void deactivate() {
+        mLocationListener=null;
+        if (mLocationClient != null) {
+            mLocationClient.stopLocation();
+            mLocationClient.onDestroy();
+        }
+        mLocationClient = null;
+    }
+
 
 }
