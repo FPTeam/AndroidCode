77998c6731606355759a47602616b3cdaaa01f1a
StayMeBYp <57319136+StayMeBYp@users.noreply.github.com>
2019-12-18 00:51:32 +0800

    隐私设置

--- littleworld/app/src/main/java/com/example/littleworld/SettingsPrivacyActivity.java
+++ littleworld/app/src/main/java/com/example/littleworld/SettingsPrivacyActivity.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.example.littleworld;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,23 +17,4 @@ public class SettingsPrivacyActivity extends AppCompatActivity {
 
     }
 }
-=======
-package com.example.littleworld;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class SettingsPrivacyActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_privacy);
-
-
-
-    }
-}
->>>>>>> 09514ba4d435255ac3941d8162c72856e9b26780
