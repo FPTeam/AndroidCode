5c239889f1a6093904e3a9afc7eeae794e304f75
nickhunie <1250023124@qq.com>
2019-12-17 23:48:23 +0800

    Update DbHelper

--- littleworld/app/src/main/java/com/example/littleworld/Adapter/passageAdapter.java
+++ littleworld/app/src/main/java/com/example/littleworld/Adapter/passageAdapter.java
@@ -14,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.littleworld.Entity.passage;
