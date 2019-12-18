fcff24a8a15f3c4bf1448508a89c4cad13fa72d5
cloudyandyou <cloudyandyou@users.noreply.github.com>
2019-12-19 00:35:12 +0800

    新增修改用户信息界面

---
+++ littleworld/app/src/main/java/com/example/littleworld/SettingEditInfo.java
@@ -0,0 +1,53 @@
+package com.example.littleworld;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingEditInfo extends AppCompatActivity {
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_edit_info);
+
+        final EditText newName = findViewById(R.id.editInfoUserName);
+        final EditText newIntroduction = findViewById(R.id.editTextIntroduction);
+
+        Button affirmBtn = findViewById(R.id.editInfoAffirmBtn);
+        affirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inName = newName.getText().toString();
+                String inIntroduction = newIntroduction.getText().toString();
+                int rep=DbHelper.getInstance().repUser(inName);
+                if(rep!=-1) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "该用户名已被占用", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                }else{
+                    Toast toast = Toast.makeText(getApplicationContext(), "修改成功！", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                    DbHelper.getInstance().insertUserInfo(190001,inName,inIntroduction,"男",null);
+                    new Handler().postDelayed(new Runnable() {
+                        public void run() {
+                                /*Intent  intent=new Intent(RegActivity.this, LoginActivity.class);
+                                startActivity(intent);*/
+                            SettingEditInfo.this.finish();
+                        }
+                    }, 2000);
+                }
+            }
+        });
+    }
+}
+
+//created by ttl
