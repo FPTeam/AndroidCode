4e6609670acd64780603684afb2bcab5a4f54c28
StayMeBYp <staymebyp@outlook.com>
2019-12-20 16:25:30 +0800

    Plus

--- littleworld/app/src/main/java/com/example/littleworld/SquareActivity.java
+++ littleworld/app/src/main/java/com/example/littleworld/SquareActivity.java
@@ -18,6 +18,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.littleworld.Adapter.SquarePagerAdapter;
