2fd8b8159b39cc878397caa7485c67a14d3347a6
HElenZWHU <43406963+HElenZWHU@users.noreply.github.com>
2019-12-21 00:56:47 +0800

    Update

--- littleworld/app/src/main/java/com/example/littleworld/Adapter/passageAdapter.java
+++ littleworld/app/src/main/java/com/example/littleworld/Adapter/passageAdapter.java
@@ -21,6 +21,7 @@ import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.littleworld.BottomMenu.NavigationActivity;
+import com.example.littleworld.DbHelper;
 import com.example.littleworld.Entity.passage;
 import com.example.littleworld.LoginActivity;
 import com.example.littleworld.ProfileActivity;
