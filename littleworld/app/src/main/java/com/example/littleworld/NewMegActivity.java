af2ebe196899143e7664b9d33c93a1f97942f073
StayMeBYp <staymebyp@outlook.com>
2019-12-20 16:27:45 +0800

    Plus

--- littleworld/app/src/main/java/com/example/littleworld/NewMegActivity.java
+++ littleworld/app/src/main/java/com/example/littleworld/NewMegActivity.java
@@ -25,29 +25,29 @@ public class NewMegActivity extends Fragment {
         this.userId = userId;
     }
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_notice);
-        layout = inflater.inflate(R.layout.activity_notice, container, false);
-        initUser();
-        UserAdapter adapter = new UserAdapter(getActivity(), R.layout.user_item, userList);
-        ListView listView = layout.findViewById(R.id.list_view);
-        listView.setAdapter(adapter);
-
-        return layout;
-    }
-
-    private void initUser(){
-        for (int i = 0; i < 2; i++){
-            User one = new User(001, R.drawable.white_bear);
-            userList.add(one);
-
-            User two = new User(002, R.drawable.white_bear);
-            userList.add(two);
-        }
-    }
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//
+//        super.onCreate(savedInstanceState);
+////        setContentView(R.layout.activity_notice);
+//        layout = inflater.inflate(R.layout.activity_notice, container, false);
+//        initUser();
+//        UserAdapter adapter = new UserAdapter(getActivity(), R.layout.user_item, userList);
+//        ListView listView = layout.findViewById(R.id.list_view);
+//        listView.setAdapter(adapter);
+//
+//        return layout;
+//    }
+//
+//    private void initUser(){
+//        for (int i = 0; i < 2; i++){
+//            User one = new User(001, R.drawable.white_bear);
+//            userList.add(one);
+//
+//            User two = new User(002, R.drawable.white_bear);
+//            userList.add(two);
+//        }
+//    }
 
 
 }
